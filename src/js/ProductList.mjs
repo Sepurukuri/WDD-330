3b34a0bc501bf9f411b36a18753ed9c8e42ed714
Santiago Cuadrado <santiagocuadradosud247@gmail.com>
2026-05-23 19:31:01 -0300

    week3 team activity

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -6,7 +6,7 @@ function productCardTemplate(product) {
       <a href="product_pages/?products=${product.Id}">
         <img src="${product.Image}" alt="${product.Name}">
         <h2>${product.Brand.Name}</h2>
-        <h3>${product.Name}</h3>
+        <h3>${product.NameWithoutBrand}</h3>
         <p class="product-card__price">$${product.FinalPrice}</p>
       </a>
     </li>
@@ -26,6 +26,10 @@ export default class ProductList {
   }
 
   renderList(list) {
+    // const htmlStrings = list.map(productCardTemplate);
+    // this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
+
+    // apply use new utility function instead of the commented code above
     renderListWithTemplate(productCardTemplate, this.listElement, list);
 
   }
