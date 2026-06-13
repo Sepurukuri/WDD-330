f23ad101748a49e0d698533f855cac0ceb8dcffc
Santiago Cuadrado <santiagocuadradosud247@gmail.com>
2026-06-13 18:25:18 -0300

    week6

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/WDD-330/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        home: resolve(__dirname, "src/home/index.html"),
+        details: resolve(__dirname, "src/details/index.html"),
+        favorites: resolve(__dirname, "src/favorites/index.html"),
+      },
+    },
+  },
 });
